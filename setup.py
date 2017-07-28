1bccb2435f735522399f20698a133348729e45f0
Jose Luis Salazar Gonzalez <joseluis25sg@gmail.com>
2017-07-28 22:08:09 +0200

    Add locale to setup

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup
 
 setup(name='teamSpeakTelegram',
-      version='0.4.1',
+      version='0.4.2',
       description='A telegram bot that tells you who is connected to the teamspeak server to know when your friends are online.',
       url='https://github.com/jossalgon/teamSpeakTelegram',
       author='Jose Luis Salazar Gonzalez',
       author_email='joseluis25sg@gmail.com',
       packages=['teamSpeakTelegram'],
+      package_data={'teamSpeakTelegram': ['locale/es/LC_MESSAGES/*']},
       install_requires=[
           "python-telegram-bot",
           "ts3",
